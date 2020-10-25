8ec243bf8b7d26e8043ac38e81507b2d1a5ed275
ViggoLagerstedtEkholm <viggo.lagerstedtekholm@gmail.com>
2020-10-25 02:46:22 +0200

    Added suppport for asynchronously fetching and update all data in the given interval.

--- DAL/Repositories/PodcastRepository.cs
+++ DAL/Repositories/PodcastRepository.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DAL.Repositories
 {
@@ -18,6 +19,7 @@ namespace DAL.Repositories
         public override void Create(Podcast entity)
         {
             base.Create(entity);
+            Console.WriteLine("Length: " + list.Count);
         }
         public override void Delete(Podcast entity)
         {
@@ -31,13 +33,35 @@ namespace DAL.Repositories
         {
             return base.GetAll();
         }
-        public void Update(string newName, int interval, Category category, int index)
+        public void Update(string URL, string newName, int interval, Category category, int index)
         {
-            list[index].name= newName;
+            list[index].URL = URL;
+            list[index].name = newName;
             list[index].category = category;
             list[index].updatingInterval = interval;
         }
-
+        public async Task Update(List<Podcast> podcasts, IProgress<int> progress, ListBox console)
+        {
+            List<List<Episode>> episodes = new List<List<Episode>>();
+            List<Episode> updatedEpisodes = new List<Episode>();
+            foreach(Podcast podcast in podcasts)
+            {
+                updatedEpisodes = await Task.Run (() => reader.GetEpisodes(podcast.URL, progress));
+                episodes.Add(updatedEpisodes);
+                console.Items.Add($"{podcast.URL} downloaded: {updatedEpisodes.Count} characters long.\n");
+                progress.Report(episodes.Count * 100 / podcasts.Count);
+                foreach(Podcast aPodcast in GetAll())
+                {
+                    if (aPodcast.URL.Equals(podcast.URL))
+                    {
+                        aPodcast.episodes = updatedEpisodes;
+                        aPodcast.amountOfEpisodes = reader.GetAmountOfEpisodes(podcast.URL);
+                    }
+                }
+                SaveChanges();
+                Console.WriteLine("Name: " + podcast.name + "Interval " + podcast.updatingInterval);
+            }
+        }
         public async void Update(string URL, IProgress<int> progress)
         {
             List<Episode> updatedEpisodes = await reader.GetEpisodes(URL, progress);
@@ -65,6 +89,10 @@ namespace DAL.Repositories
 
         public void SaveChanges()
         {
+            foreach(Podcast podcast in list)
+            {
+                Console.WriteLine(podcast.URL);
+            }
             objectSerializer.Serialize(list, Constants.podcast.Value, true);
         }
 
