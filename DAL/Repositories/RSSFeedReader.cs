2d50a2418d1076ce9b5c7a0ec1a1f7b9d7affb1d
ViggoLagerstedtEkholm <viggo.lagerstedtekholm@gmail.com>
2020-10-26 18:51:46 +0100

    Added custom exceptions.

--- DAL/Repositories/RSSFeedReader.cs
+++ DAL/Repositories/RSSFeedReader.cs
@@ -3,79 +3,75 @@ using Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.Remoting.Messaging;
 using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
 
 namespace DAL
 {
-    //Delegate here?
     public class RSSFeedReader
     {
-        public RSSFeedReader()
-        {}
-        public string GetTitle(string url)
-        {
-            XmlReader xmlReader = XmlReader.Create(url);
-            SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
-
-            return feed.Title.Text;
-        }
-        public string GetDescription(string url)
-        {
-            XmlReader xmlReader = XmlReader.Create(url);
-            SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
-
-            return feed.Description.Text;
-        }
         public int GetAmountOfEpisodes(string url)
         {
             XDocument urlDocument = new XDocument();
             int amountOfEpisodes = 0;
 
-            urlDocument = XDocument.Load(url);
-            var items = (from x in urlDocument.Descendants("item")select new 
-                        { 
-                            title = x.Element("title") 
-                        });
-
-            if (items != null)
+            try
             {
-                foreach (var i in items)
+                urlDocument = XDocument.Load(url);
+                var items = (from x in urlDocument.Descendants("item")
+                             select new
+                             {
+                                 title = x.Element("title")
+                             });
+
+                if (items != null)
                 {
-                    amountOfEpisodes++;
+                    foreach (var i in items)
+                    {
+                        amountOfEpisodes++;
+                    }
                 }
+
             }
+            catch (ExceptionHandle)
+            {
+                throw new ExceptionHandle("Could not get amount of episodes");
+            }
+
             return amountOfEpisodes;
         }
 
         //https://www.c-sharpcorner.com/UploadFile/70dbe6/fetch-rss-feed-content-from-linq-to-xml/
-        public async Task<List<Episode>> GetEpisodes(string url, IProgress<int> progress)
+        public async Task<List<Episode>> GetEpisodes(string url)
         {
-           
             List<Episode> episodeList = new List<Episode>();
-
             XDocument urlDocument = new XDocument();
-            urlDocument = XDocument.Load(url);
-            await Task.Run(() =>
+
+            try
             {
-                episodeList = (from x in urlDocument.Descendants("item")
+                urlDocument = XDocument.Load(url);
+                await Task.Run(() =>
+                {
+                    episodeList = (from x in urlDocument.Descendants("item")
+                                   select new Episode
+                                   {
+                                       Name = x.Element("title").Value,
+                                       Description = x.Element("description").Value,
+                                       SoundFile = x.Descendants("link").Any() ? x.Element("link").Value : x.Descendants("enclosure").Any() ? x.Element("enclosure").Attribute("url").Value : "None"
+                                   }).ToList();
+                });
+            }
+            catch (ExceptionHandle)
+            {
+                throw new ExceptionHandle("Could not get episodes.");
+            }
 
-                               select new Episode
-                               {
-                                   
-                                   name = x.Element("title").Value,
-                                   description = x.Element("description").Value,
-
-                                   soundFile = x.Descendants("link").Any() ? x.Element("link").Value : x.Descendants("enclosure").Any() ? x.Element("enclosure").Attribute("url").Value : "None"
-                                   
-
-                               }).ToList();
-            });
-            
 
             return episodeList;
         }
